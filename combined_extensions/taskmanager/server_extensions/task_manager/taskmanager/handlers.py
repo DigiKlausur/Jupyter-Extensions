1aefb35e2956d0a1093c55d3e4085ffa67ec5c94
Tim Metzler <tim.metzler@h-brs.de>
2019-08-12 14:53:50 +0200

    Clean up taskmanager

--- combined_extensions/taskmanager/server_extensions/task_manager/taskmanager/handlers.py
+++ combined_extensions/taskmanager/server_extensions/task_manager/taskmanager/handlers.py
@@ -1,16 +1,5 @@
 from .base import BaseHandler
-import os
-
-class HbrsGraderBaseHandler(BaseHandler):
-
-    def get(self):
-        html = self.render('base.tpl', title='Wooohooo!')
-        self.write(html)
-
-
-root_path = os.path.dirname(__file__)
-template_path = os.path.join(root_path, 'templates')
 
 default_handlers = [
-    (r"/hbrs_grader", HbrsGraderBaseHandler),
+    
 ]
--- combined_extensions/taskmanager/server_extensions/task_manager/taskmanager/templates/base.tpl
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<head>
-  <title>HBRS Grader</title>
-</head>
-
-<body>
-  <h1>{{ title }}</h1>
-</body>
